de433fd5c34438de8a42c56a724a5f94e987ed05
Mike Jessup <jessupmr@gmail.com>
2020-01-03 13:03:38 -0700

    Made recommended changes from code inspection.

--- src/dev/jessup/Main.java
+++ src/dev/jessup/Main.java
@@ -1,8 +1,13 @@
 package dev.jessup;
 
+import dev.jessup.util.ConsoleLogger;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello Java!");
+
+        ConsoleLogger log = new ConsoleLogger();
+        log.header("Main.main()");
+        log.message("Hello IDEA and Git");
     }
 }
